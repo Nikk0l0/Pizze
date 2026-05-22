36e793cb6367aa9d3230526db9eef3baf7abc2c8
Niccolò <niccolo.m.visentin@gmail.com>
2026-05-22 09:46:01 +0200

    Pizza patatosa

--- pizze.py
+++ pizze.py
@@ -7,6 +7,22 @@ class Pizza:
     def area(self):
         return  (self.diametro / 2 ) ** 2 * math.pi
 
+    def __str__(self):
+        return f"Pizza {self.diametro} di diametro"
+
+class Patatosa(Pizza):
+    def __init__(self, diametro, patate):
+        super().__init__(diametro)
+        self.patate = patate
+
+    def __str__(self):
+        return (super().__str__ + f", patate ")
+
+    
+
 if __name__ == "__main__":
     prima = Pizza(4)
     print(prima.area())
+
+    seconda = Patatosa(5, 12)
+    print(seconda)
